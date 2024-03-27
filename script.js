62de70cebbdc10a456de4b6dfe4f443336790794
Joaquim-Azevedo <joaquimazeveddoo@gmail.com>
2024-03-27 20:38:01 -0300

    Mod alert e if

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ const meuNome = document.getElementById('meuNome');
 const datadeNamoro = document.querySelector('input[type="date"]');
 const validarbtn = document.getElementById('validar');
 
-
-
 validarbtn.addEventListener('click', prosseguir)
 function prosseguir(){
     var nome = meuNome.value;
     var data = datadeNamoro.value;
-    console.log(data)
-    if(nome == "Maria" || nome == "maria" && data == "2021-09-18"){
+
+    if(nome == "Maria" || nome == "maria" || nome == "Maria Clara" || nome == "maria clara" && data == "2021-09-18"){
         alert('Deu certo!')
         validar()
     } else {
-        alert('Algo está errado, verifique suas respostas!')
+        alert('Algo está errado, verifique suas respostas!\n\nUse seu primeiro e segundo nome ou apenas o primeiro.')
     }
     
 }
